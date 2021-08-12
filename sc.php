65d41a638b55c41d6ea4fbef31c8c8f4fb0a0ba2
Kevin <79812923+krvinc0de@users.noreply.github.com>
2021-08-11 19:09:49 -0500

    Versión 1.0
    Primera versión de la pagina web de xidoo rutas

---
+++ sc.php
@@ -0,0 +1,23 @@
+<?php
+$email = $_POST['email'];
+$nombre = $_POST['nombre'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message .= "Su correo electronico es: " . $email . " \r\n";
+$message .= "\r\n";
+$message .= "Enviado el: " . date('d/m/Y', time()."\r\n");
+$message .= "\r\n";
+$message .= "\r\nPor favor, avisale:)";
+
+$para = 'soporte@xidoorutas.codes';
+$asunto = 'A ' . $nombre . ' le interesa la aplicacion.';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:gracias");
+?>
---
+++ scontact.php
@@ -0,0 +1,26 @@
+<?php
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['asunto'];
+$email = $_POST['correo'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Su numero de telefono es: " . $telefono . " \r\n";
+$message .= "\r\n";
+$message .= $nombre . " quiere decir: ". $asunto . " \r\n";
+$message .= "\r\n";
+$message .= "Enviado el: " . date('d/m/Y', time()."\r\n");
+
+$para = 'soporte@xidoorutas.codes';
+$asunto = 'El usuario: '. $nombre . ' quiere contactarnos!';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:recibido");
+?>
